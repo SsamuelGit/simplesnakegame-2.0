009c363790795f2f13ba206c66ec6fedf5e898ae
Samuel Kreft <samuel.kreft13@gmail.com>
2024-04-27 10:12:48 +0200

    create the canvas & basic structure

--- game.js
+++ game.js
@@ -1,4 +1,25 @@
 const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-context.fillStyle = "black";
-context.fillRect(0, 0, 480, 480);
+let context = canvas.getContext("2d");
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+//set canvas size to screen size
+window.onload = function() {
+    const canvas = document.getElementById("canvas");
+    window.onresize = function() {
+        canvas.style.width = window.innerWidth + 'px';
+        canvas.style.height = window.innerHeight + 'px';
+    }
+    canvas.style.width = window.innerWidth + 'px';
+    canvas.style.height = window.innerHeight + 'px';
+}
+
+
+draw();
+
+function gameLoop() {
+
+}
+function draw() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
