3678a5c7f23ec78e95d2fab5ea7160f3ac13f41e
Samuel Kreft <samuel.kreft13@gmail.com>
2024-04-27 18:06:29 +0200

    render the canvas

--- game.js
+++ game.js
@@ -1,24 +1,25 @@
+//create necessary variables
 const canvas = document.getElementById("canvas");
-let context = canvas.getContext("2d");
+const context = canvas.getContext("2d");
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
-//set canvas size to screen size
+let rows = 16
+
+//set canvas size to screen size * 3/4
 window.onload = function() {
-    const canvas = document.getElementById("canvas");
-    window.onresize = function() {
-        canvas.style.width = window.innerWidth + 'px';
-        canvas.style.height = window.innerHeight + 'px';
-    }
-    canvas.style.width = window.innerWidth + 'px';
-    canvas.style.height = window.innerHeight + 'px';
+    canvas.style.width = (window.innerWidth / 4 * 3) + 'px';
+    canvas.style.height = (window.innerHeight / 4 * 3)  + 'px';
 }
 
-
+//call the functions so that the game works
 draw();
 
+//very important function to calculate the game
 function gameLoop() {
 
 }
+
+//very important function to draw the game
 function draw() {
     context.fillStyle = "black";
     context.fillRect(0, 0, screenWidth, screenHeight);
