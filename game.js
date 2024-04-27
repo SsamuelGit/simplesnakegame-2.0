ba59cf8475ebd2427fc1911cee91cef911cc0a9a
Samuel Kreft <samuel.kreft13@gmail.com>
2024-04-27 20:23:54 +0200

    (bug) calculate rows and columns function

--- game.js
+++ game.js
@@ -1,15 +1,17 @@
-//create necessary variables
+
+//render the canvas (set canvas size to screen size * 3/4)
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-const screenWidth = window.innerWidth;
-const screenHeight = window.innerHeight;
-let rows = 16
-
-//set canvas size to screen size * 3/4
 window.onload = function() {
     canvas.style.width = (window.innerWidth / 4 * 3) + 'px';
     canvas.style.height = (window.innerHeight / 4 * 3)  + 'px';
 }
+
+//create necessary variables
+const rows = calculateCellProperties("row");
+const columns = calculateCellProperties("column"); 
+console.log(rows);
+console.log(columns);
 
 //call the functions so that the game works
 draw();
@@ -23,4 +25,24 @@ function gameLoop() {
 function draw() {
     context.fillStyle = "black";
     context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+//other functions
+function calculateCellProperties(sizeToBeCalculated) {
+    if (sizeToBeCalculated == "row") {
+        let cellHeight = 1;
+        let numberOfRows;
+        for (numberOfRows = 500; cellHeight < 30; numberOfRows--) {
+            cellHeight = canvas.style.height / numberOfRows;
+        }
+        return numberOfRows;
+    }
+    if (sizeToBeCalculated == "column") {
+        let cellWidth = 1;
+        let numberOfColumns;
+        for (numberOfColumns = 500; cellWidth < 30; numberOfColumns--) {
+            cellWidth = canvas.style.width / numberOfColumns;
+        }
+        return numberOfColumns;
+    }
 }
