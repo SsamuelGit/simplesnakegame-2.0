e7ca55fe2b4d716a698a0cf27b1f6e562e5f135d
Samuel Kreft <samuel.kreft13@gmail.com>
2024-04-28 15:03:24 +0200

    change to ES6 default

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+const img = document.getElementById('bgImg');
 window.onload = function() {
-    var img = document.getElementById('bgImg');
-    window.onresize = function() {
-        img.style.width = window.innerWidth + 'px';
-        img.style.height = window.innerHeight + 'px';
-    }
+    img.style.width = window.innerWidth + 'px';
+    img.style.height = window.innerHeight + 'px';
+}
+window.onresize = function() {
     img.style.width = window.innerWidth + 'px';
     img.style.height = window.innerHeight + 'px';
 }
